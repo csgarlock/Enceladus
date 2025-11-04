8d07b8929f30e5fd2e18fbe3ef5b1f85bcf231e4
csgarlock <csgarlock@gmail.com>
2025-11-04 11:52:22 -0500

    Stuff

---
+++ src/types/simple_model.cuh
@@ -0,0 +1,18 @@
+#ifndef SIMPLE_MODEL_H_INCLUDED
+#define SIMPLE_MODEL_H_INCLUDED
+
+#include "tensor.cuh"
+
+class SimpleModel {
+
+    public:
+
+    Tensor<float> masses;
+    Tensor<float3> positions;
+    Tensor<float3> velocities;
+
+    float dt;
+
+};
+
+#endif
